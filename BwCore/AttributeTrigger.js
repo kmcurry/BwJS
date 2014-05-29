a6722c6c096057598c9e0b2b2163d164baa2c7eb
Kevin Curry <kevin@codeforamerica.org>
2014-05-29 14:02:50 -0400

    first commit

---
+++ BwCore/AttributeTrigger.js
@@ -0,0 +1,144 @@
+AttributeTrigger.prototype = new Command();
+AttributeTrigger.prototype.constructor = AttributeTrigger;
+
+
+function AttributeTrigger(input, trigger, target, item, _not, _executionCount)
+{
+    this.input = input;
+    this.trigger = trigger;
+    this.target = target;
+    
+	this.input.getValue(this.lastValues);
+	
+    this.item = item;
+    
+    this.not = _not;
+    this.executionCount = _executionCount;
+    
+    executionCount = new UnsignedIntegerAttr(this.executionCount);
+
+	this.input.addRef();
+
+	this.input.addModifiedSink(AttributeTrigger_InputModifiedCB, this);
+
+	this.target.setUndoable(false);
+
+	this.input.getValue(this.lastValues);
+
+	var len = this.input.getLength();
+
+	if (len == 1)
+	{
+		this.item = 0;
+	}
+}
+
+
+AttributeTrigger.prototype.execute = function()
+{
+	eERR_CODE err = eERR_FAIL;
+
+	if (this.target)
+	{
+        eAttrElemType type = this.trigger.getAttributeElementType();
+        switch (type)
+        {
+        case eAttrElemType_Char:
+            {
+                var vIn;
+                var vTrig;
+            
+                this.input.getValue(vIn);
+                this.trigger.getValue(vTrig);
+
+                var pass = vIn[0] == vTrig[0] ? true : false;
+                pass = this.not ? !pass : pass;
+                if (pass)
+                {
+					err = this.target.execute();
+					executionCount.setValueDirect(--this.executionCount);
+                }
+
+                if (this.executionCount == 0)
+		        {
+			        this.target = NULL;
+		        }
+            }
+            break;
+
+        default:
+            {
+                var vIn;
+                var vTrig;
+
+		        this.input.getValue(vIn);
+		        this.trigger.getValue(vTrig);
+
+		        // match single-item Attribute OR single item of a multi-item Attribute
+		        if (this.item != -1)
+		        {
+			        // if equal OR descending past OR ascending past
+                    bool pass = ((vIn[this.item] == vTrig[0]) ||
+			                     (this.lastValues[this.item] > vIn[this.item] && vIn[this.item] < vTrig[0]) ||
+			                     (this.lastValues[this.item] < vIn[this.item] && vIn[this.item] > vTrig[0]));
+                    pass = this.not ? !pass : pass;
+                    if (pass)
+			        {
+						err = this.target.Execute();
+				        executionCount.setValueDirect(--this.executionCount);
+			        }
+		        }
+		        else	// match every item in a multi-item Attribute
+		        {
+			        int len = this.input.getLength();
+			        int matches = 0;
+			        for (int i = 0; i < len; ++i)
+			        {
+				        // if equal OR descending past OR ascending past
+				        var pass = ((vIn[i] == vTrig[i]) ||
+				                     (this.lastValues[i] > vIn[i] && vIn[i] < vTrig[i]) ||
+				                     (this.lastValues[i] < vIn[i] && vIn[i] > vTrig[i]));
+                        pass = this.not ? !pass : pass;
+                        if (pass)
+				        {
+					        ++matches;
+				        }
+			        }
+
+			        // if every item matches simultaneously
+			        if (matches = len)
+			        {
+						err = this.target.Execute();
+						executionCount.setValueDirect(--this.executionCount);
+			        }
+		        }
+
+		        if (this.executionCount == 0)
+		        {
+			        this.target = NULL;
+		        }
+		        else
+		        {
+			        this.lastValues = vIn;
+		        }
+            }
+            break;
+        }
+	}
+
+	return;
+}
+
+function AttributeTrigger_InputModifiedCB(attribute, container)
+{
+	container.execute();
+
+	// TODO:  Expand to also support EventListener::EventPerformed
+}
+/*
+void AttributeTrigger_InputModifiedTaskProc(void* data, const bool & run)
+{
+	TAttributeTrigger* pTrigger = static_cast<TAttributeTrigger*>(data);
+	pTrigger.Execute();
+}
+*/
