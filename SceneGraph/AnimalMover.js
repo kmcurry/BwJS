f3469284e5721ddb32886274b1f32c5d683c8172
Michael Chase Brazell <mbraz003@gmail.com>
2014-12-30 10:50:25 -0500

    Update animal mover to match Collision-Updates branch.

--- SceneGraph/AnimalMover.js
+++ SceneGraph/AnimalMover.js
@@ -8,17 +8,19 @@ function AnimalMover()
     ObjectMover.call(this);
     this.className = "AnimalMover";
     this.attrType = eAttrType.AnimalMover;
+    
+    this.linearDirection = null;
 }
 
 AnimalMover.prototype.evaluate = function()
 {
-	if (this.motionQueue.length() < ANIMALMOVER_MAX_QUEUE_LENGTH)
+	if (this.motionQueue.length() == 0)
 	{
-		var motion = new ObjectMotionDesc();
-		motion.duration = FLT_MAX;
-		motion.panVelocity = new Vector3D(0, 0, this.linearSpeed.getValueDirect());
+	    var walk = new ObjectMotionDesc();
+		walk.duration = FLT_MAX;
+		walk.panVelocity = new Vector3D(0, 0, this.linearSpeed.getValueDirect());
 		
-		this.motionQueue.push(motion);
+		this.motionQueue.push(walk);
 	}
 	
 	// call base-class implementation
@@ -27,18 +29,42 @@ AnimalMover.prototype.evaluate = function()
 
 AnimalMover.prototype.collisionDetected = function(collisionList)
 {   
-    this.activeMotion = null;
-    this.motionQueue.clear();
-    
-    var turn = new ObjectMotionDesc();
-    turn.duration = 1 / this.angularSpeed.getValueDirect();
-    turn.angularVelocity = new Vector3D(0, this.angularSpeed.getValueDirect(), 0);
-        
-    this.motionQueue.push(turn);
-      
-    var walk = new ObjectMotionDesc();
-    walk.duration = 1 / this.linearSpeed.getValueDirect();
-    walk.panVelocity = new Vector3D(0, 0, this.linearSpeed.getValueDirect());
-        
-    this.motionQueue.push(walk);
+    if (collisionList.Size() > 0) // collision(s) occurred
+    {
+       this.motionQueue.clear();
+       this.activeMotion = null;
+
+       // determine vector to reverse collision by subtracting collider position(s) from this position
+       var thisPos = this.targetObject.getAttribute("sectorPosition").getValueDirect();
+       var linearDirection = new Vector3D(0, 0, 0);
+       for (var i=0; i < collisionList.Size(); i++)
+       {
+           var colliderPos = collisionList.getAt(i).getAttribute("sectorPosition").getValueDirect();
+           var deltaPos = new Vector3D(thisPos.x - colliderPos.x, /*thisPos.y - colliderPos.y*/0, thisPos.z - colliderPos.z);
+           linearDirection.addVector(deltaPos);
+       }
+       linearDirection.normalize();
+       
+       // scale by linear speed   
+       linearDirection.multiplyScalar(this.linearSpeed.getValueDirect());
+       this.linearDirection = linearDirection;
+       
+       // push linear velocity
+       var linear = new ObjectMotionDesc();
+       linear.linearVelocity = linearDirection;
+       linear.duration = FLT_MAX;
+       this.motionQueue.push(linear);
+       
+       // turn so this will travel in direction of this vector
+       var directionVectors = this.targetObject.getDirectionVectors();
+       var angleBetween = toDegrees(Math.acos(cosineAngleBetween(directionVectors.forward, this.linearDirection)));
+       if (angleBetween > 0)
+       {
+           var rotation = this.targetObject.getAttribute("rotation").getValueDirect();
+           this.targetObject.getAttribute("rotation").setValueDirect(rotation.x, 360 - angleBetween + rotation.y, rotation.z);
+       }
+    }
+    else // no collision(s)
+    {
+    }
 }
