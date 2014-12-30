c14ebca303236f5cc90b176e2a151a5641e01557
Michael Chase Brazell <mbraz003@gmail.com>
2014-12-30 10:07:39 -0500

    Updated to work w/ 3Scape

--- BwCore/ScreenCapture.js
+++ BwCore/ScreenCapture.js
@@ -24,21 +24,22 @@ ScreenCaptureCommand.prototype.execute = function()
 ScreenCaptureCommand.prototype.screenCapture = function(canvasId)
 {
     var canvas = document.getElementById(canvasId);
-
     var imageData = canvas.toDataURL('image/png');
-    //window.open(imageData);
-    document.getElementById('imgCapture').src = imageData;
 
-    //Decode the base64 data into 8bit array. Used Specifically for 3Scape
-    //var cnt = imageData.lastIndexOf(',') + 1;
-    //imageData = imageData.substr(cnt);
-    //imgeData = Base64Binary.decode(imageData);
+    // 3Scape-specific: decode the base64 data into 8bit array
+    var cnt = imageData.lastIndexOf(',') + 1;
+    imageData = imageData.substr(cnt);
+    imgeData = Base64Binary.decode(imageData);
+        
+    // for testing with Bug-60.htm
+    //document.getElementById('imgCapture').src = imageData;
 
     // copy to clipboard
     // TODO: investigate method described at: https://forums.mozilla.org/addons/viewtopic.php?t=9736&p=21119
 
     // open in new window
-
+    //window.open(imageData);
+    
     // download
     //var imageDataStream = imageData.replace("image/png", "image/octet-stream");
     //window.location.href = imageDataStream;
