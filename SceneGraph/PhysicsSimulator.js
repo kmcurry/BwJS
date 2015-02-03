ac246740d72991175ddad256fae2639f299b402a
Michael Chase Brazell <mbraz003@gmail.com>
2015-02-02 19:19:36 -0500

    Added object inspection support.

--- SceneGraph/PhysicsSimulator.js
+++ SceneGraph/PhysicsSimulator.js
@@ -9,6 +9,8 @@ function PhysicsSimulator()
 
     this.world = null;
     this.physicsBodies = [];
+    this.physicsShapes = [];
+    this.bodyAdded = [];
     this.bodyModels = [];
     this.updateWorld = false;
     this.updateBodies = false;
@@ -39,11 +41,40 @@ PhysicsSimulator.prototype.evaluate = function()
         this.updateWorld = false;
         this.initPhysics();
     }
-    
+
     if (this.updateBodies)
     {
         this.updateBodies = false;
         this.updatePhysicsBodies();
+    }
+
+    // add/remove bodies based on selection state (allows for object inspection)
+    for (var i = 0; i < this.bodyModels.length; i++)
+    {
+        switch (this.bodyModels[i].getAttribute("selected").getValueDirect())
+        {
+            case 0: // unselected
+                {
+                    // if not added, restore
+                    if (!this.bodyAdded[i])
+                    {
+                        this.restorePhysicsBody(i);
+                        this.bodyAdded[i] = true;
+                    }
+                }
+                break;
+
+            case 1: // selected
+                {
+                    // if added, remove
+                    if (this.bodyAdded[i])
+                    {
+                        this.world.removeRigidBody(this.physicsBodies[i]);
+                        this.bodyAdded[i] = false;
+                    }
+                }
+                break;
+        }
     }
 
     var timeIncrement = this.timeIncrement.getValueDirect();
@@ -52,9 +83,12 @@ PhysicsSimulator.prototype.evaluate = function()
     var trans = new Ammo.btTransform();
     for (var i = 0; i < this.physicsBodies.length; i++)
     {
-        this.physicsBodies[i].getMotionState().getWorldTransform(trans);
-        var position = new Vector3D(trans.getOrigin().x().toFixed(2), trans.getOrigin().y().toFixed(2), trans.getOrigin().z().toFixed(2));
+        if (!this.bodyAdded[i]) continue;
         
+        this.physicsBodies[i].getMotionState().getWorldTransform(trans);
+        var origin = trans.getOrigin();
+        var position = new Vector3D(origin.x(), origin.y(), origin.z());
+
         var rot = trans.getRotation();
         var quat = new Quaternion();
         quat.load(rot.w(), rot.x(), rot.y(), rot.z());
@@ -69,6 +103,8 @@ PhysicsSimulator.prototype.updatePhysicsBodies = function()
     // reset bodies
     this.initPhysics();
     this.physicsBodies = [];
+    this.physicsShapes = [];
+    this.bodyAdded = [];
     this.bodyModels = [];
 
     // add bodies to world
@@ -79,40 +115,92 @@ PhysicsSimulator.prototype.updatePhysicsBodies = function()
             continue;
         this.bodyModels.push(body);
 
-        var physicalProperties = body.getAttribute("physicalProperties");
-        var mass = physicalProperties.getAttribute("mass").getValueDirect();
-
         var colShape = new Ammo.btConvexHullShape();
         var verts = body.getAttribute("vertices").getValueDirect();
         for (var i = 0; i < verts.length; i += 3)
         {
             colShape.addPoint(new Ammo.btVector3(verts[i], verts[i + 1], verts[i + 2]));
-        }       
-        
+        }
+        this.physicsShapes.push(colShape);
+
         var startTransform = new Ammo.btTransform();
         startTransform.setIdentity();
 
         var position = body.getAttribute("sectorPosition").getValueDirect();
         startTransform.setOrigin(new Ammo.btVector3(position.x, position.y, position.z));
-        
+
         var rotation = body.getAttribute("rotation").getValueDirect();
         var quat = new Quaternion();
         quat.loadXYZAxisRotation(rotation.x, rotation.y, rotation.z);
         startTransform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+
         // TODO: scale
+
+        var physicalProperties = body.getAttribute("physicalProperties");
+        var mass = physicalProperties.getAttribute("mass").getValueDirect();
         
         var isDynamic = (mass != 0);
         var localInertia = new Ammo.btVector3(0, 0, 0);
         if (isDynamic)
             colShape.calculateLocalInertia(mass, localInertia);
-        
+
         var motionState = new Ammo.btDefaultMotionState(startTransform);
         var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia);
         var body = new Ammo.btRigidBody(rbInfo);
 
         this.world.addRigidBody(body);
         this.physicsBodies.push(body);
+        this.bodyAdded.push(true);
     }
+}
+
+PhysicsSimulator.prototype.restorePhysicsBody = function(n)
+{
+    var body = this.bodyModels[n];
+    var shape = this.physicsShapes[n];
+    
+    var startTransform = new Ammo.btTransform();
+    startTransform.setIdentity();
+
+    var position = body.getAttribute("sectorPosition").getValueDirect();
+    startTransform.setOrigin(new Ammo.btVector3(position.x, position.y, position.z));
+
+    // update rotation to include rotation caused by object inspection
+    var rotationGroup = getInspectionGroup(body);
+    if (rotationGroup)
+    {
+        var rotQuat = rotationGroup.getChild(2).getAttribute("rotationQuat").getValueDirect();
+        var quat1 = new Quaternion();
+        quat1.load(rotQuat.w, rotQuat.x, rotQuat.y, rotQuat.z);
+        
+        var bodyQuat = body.getAttribute("quaternion").getValueDirect();
+        var quat2 = new Quaternion();
+        quat2.load(bodyQuat.w, bodyQuat.x, bodyQuat.y, bodyQuat.z);
+        
+        var quat = quat2.multiply(quat1);
+        
+        startTransform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+                    
+        // clear inspection group's rotation
+        rotationGroup.getChild(2).getAttribute("rotationQuat").setValueDirect(new Quaternion());
+    }
+
+    // TODO: scale
+
+    var physicalProperties = body.getAttribute("physicalProperties");
+    var mass = physicalProperties.getAttribute("mass").getValueDirect();
+        
+    var isDynamic = (mass != 0);
+    var localInertia = new Ammo.btVector3(0, 0, 0);
+    if (isDynamic)
+        shape.calculateLocalInertia(mass, localInertia);
+
+    var motionState = new Ammo.btDefaultMotionState(startTransform);
+    var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
+    var body = new Ammo.btRigidBody(rbInfo);
+
+    this.world.addRigidBody(body);
+    this.physicsBodies[n] = body;
 }
 
 PhysicsSimulator.prototype.initPhysics = function()
@@ -122,7 +210,7 @@ PhysicsSimulator.prototype.initPhysics = function()
     var overlappingPairCache = new Ammo.btDbvtBroadphase();
     var solver = new Ammo.btSequentialImpulseConstraintSolver();
     this.world = new Ammo.btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
-    
+
     var gravity = this.gravity.getValueDirect();
     this.world.setGravity(new Ammo.btVector3(gravity.x, gravity.y, gravity.z));
 
